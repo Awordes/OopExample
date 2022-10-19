098213f0ce5e4ef5ef1b21d3eadbf81f1541d576
Andrew Tolstov <awordes76@gmail.com>
2022-10-19 14:47:57 +0300

    Added saving cat voice

--- Program.cs
+++ Program.cs
@@ -3,10 +3,16 @@
     public static void Main()
     {
         PrintCatVoice(); 
+        SaveCatVoice();
     }
 
     public static void PrintCatVoice()
     {
         Console.WriteLine("Meow");
+    }
+
+    public static void SaveCatVoice()
+    {
+        File.WriteAllText($"cat.voice", "Meow");
     }
 }
